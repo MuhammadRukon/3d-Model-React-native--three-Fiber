4bb7195e0e4922ca320d0baad8a87ca459f56d2e
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-09-15 18:12:55 +0600

    3d dollar sign

---
+++ assets/Model.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei/native";
+export default function Model(props) {
+  const { onLoaded } = props;
+  const { nodes, materials } = useGLTF(require("./models/dollar.glb"));
+
+  onLoaded();
+
+  return (
+    <group {...props} dispose={null} scale={0.5}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Object_2.geometry}
+        material={materials["Meshgrass1Mtl.001"]}
+        rotation={[-Math.PI / 2.2, 0, -1.5]}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload(require("./models/dollar.glb"));
